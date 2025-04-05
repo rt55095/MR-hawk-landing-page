319130f2406fca30fafd80e6dda2aac9fe66d4af
rt55095 <113915356+rt55095@users.noreply.github.com>
2025-04-05 13:49:58 -0600

    Update MrHawkLanding.jsx

--- src/MrHawkLanding.jsx
+++ src/MrHawkLanding.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+
+export default function MrHawkLanding() {
+  return (
+    <div style={{ backgroundColor: "#000", color: "#fff", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+      <h1 style={{ fontSize: "2rem", textAlign: "center" }}>Bienvenido a Mr. Hawk Bot</h1>
+      <p style={{ marginTop: "1rem", textAlign: "center" }}>Landing Page lista para tus prop traders</p>
+    </div>
+  );
+}
 
